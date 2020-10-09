1e7f3180c7f10e8aba2cdbb33e8470bf3a6b719e
lcemerald11 <59779650+lcemerald11@users.noreply.github.com>
2020-10-09 18:04:42 -0500

    Create Main.py
    Made tree function

---
+++ Main.py
@@ -0,0 +1,34 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+def tree(x,y,scale):
+  # This draws out the base of a tree. U can selsect the starting cords and the scale (0.5-2)
+  painter.color("brown")
+  painter.goto(x, y)
+  count = 0
+
+  painter.fillcolor("brown")
+  painter.begin_fill()
+  while count <= 4:
+
+    if count % 2 == 0:
+        painter.forward(3*scale)
+        painter.right(90)
+    else:
+        painter.forward(6*scale)
+        painter.right(90)
+    count += 1
+  painter.end_fill()
+
+  painter.fillcolor("green")
+  painter.color("green")
+  painter.begin_fill()
+  painter.goto((x+1.5), (y + 15))
+  painter.circle((9*scale),360,3)
+  painter.end_fill()
+
+
+
+tree(1, 2, 1)
+wn = trtl.Screen()
+wn.mainloop()
